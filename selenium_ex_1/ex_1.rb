0b1635af98b3b2df7196d532a1bb6a928acd0da9
P Paul Jonathan <70207656+ppauljonathan@users.noreply.github.com>
2022-07-06 11:54:51 +0530

    Update ex_1.rb

--- selenium_ex_1/ex_1.rb
+++ selenium_ex_1/ex_1.rb
@@ -2,10 +2,10 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to 'https://www.google.com/'
 google_search_input = driver.find_element(class: 'gLFyf')
-google_search_input.send_keys('Elon Musk')
+google_search_input.send_keys('Elon Musk') # ref: ./google_homepage_search.png
 google_search_input.submit
-sleep(5)
+sleep(5) # ref: ./google_search_results.png
 driver.find_element(partial_link_text: 'wikipedia').click
-sleep(5)
+sleep(5) # ref: ./elon_musk_wikipedia_page.png
 driver.find_element(link_text: 'business magnate').click
-sleep(5)
+sleep(5) # ref: ./business_magnate_wikipedia_page.png
